ba90f1a4088c7e4e88366838f1c591bae9615ed2
Miguel Peixe <miguel@cardume.art.br>
2015-04-27 16:08:07 -0300

    fixed content nav

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,42 @@
 require('./legend-disc')();
 require('./viz');
+
+require('fitvids/jquery.fitvids');
+
+var jQuery = $ = require('jquery');
+
+$(document).ready(function() {
+	// fit vids
+	$('body').fitVids();
+
+	// fixed content nav
+
+	(function() {
+
+		var $nav = $('#content-nav');
+		var offset = $nav.offset().top;
+		
+		var clone = false;
+
+		$(document).resize(function() {
+			offset = $nav.offset().top;
+		});
+
+		$(window).scroll(function() {
+			var scrollTop = $(window).scrollTop();
+
+			if(scrollTop >= offset) {
+				if(!clone && !clone.length) {
+					clone = $nav.clone();
+					clone.addClass('fixed').insertAfter($nav);
+				}
+			} else {
+				if(clone && clone.length) {
+					clone.remove();
+					clone = false;
+				}
+			}
+		});
+
+	})();
+});
